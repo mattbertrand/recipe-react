f8c9704d91aa21744e3c3495e91105c330be7309
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-16 15:47:56 -0700

    imported api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  
+  const APP_ID = 'aef66a74';
+  const APP_KEY = '3a3fe067527bc8fee6ca2a2bc5e17340';
+
+  const exampleRequest = `https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_0123456789abcdef0123456789abcdef&app_id=${APP_ID}&app_key=${APP_KEY}`
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
     </div>
-  );
+  )
 }
 
 export default App;
