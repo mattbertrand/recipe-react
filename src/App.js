cc753628a5a0c1123c8d22dd6c652f0bf027f229
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-18 10:53:39 -0700

    fetching api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
@@ -6,11 +6,22 @@ const App = () => {
   const APP_ID = 'aef66a74';
   const APP_KEY = '3a3fe067527bc8fee6ca2a2bc5e17340';
 
-  const exampleRequest = `https://api.edamam.com/search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_0123456789abcdef0123456789abcdef&app_id=${APP_ID}&app_key=${APP_KEY}`
+  useEffect( () => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`)
+    const data = await response.json()
+    console.log(data)
+  }
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text"></input>
+        <button className="search-button" type="submit">Search</button>
+      </form>
     </div>
   )
 }
