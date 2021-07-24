00117c3e5f78bd4e1ce287e228cea3d26f16189d
Matthieu Bertrand <matthieubertrand5@yahoo.fr>
2021-07-23 17:19:52 -0700

    finalized styling

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import style from './recipe.module.css'
 
 const Recipe = ({title, calories, image, ingredients}) => {
     return (
-        <div>
-            <h1>{title}</h1>
+        <div className={style.recipe}>
+            <h1 >{title}</h1>
             <ol>
                 {ingredients.map(ingredient => (
                     <li>{ingredient.text}</li>
                 ))}
             </ol>
             <p>{calories}</p>
-            <img src={image} alt="" />
+            <img className={style.image} src={image} alt="" />
         </div>
     )
 }
